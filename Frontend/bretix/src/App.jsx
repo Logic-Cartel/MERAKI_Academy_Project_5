b794c749aa42bd00a3c0f201f58b0614402fcc01
AhmadMAlhyari <ahmad.m.alhiyari@gmail.com>
2025-12-25 02:10:00 +0300

    frontEnd_starterCode

---
+++ Frontend/bretix/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Shared componenets/Home";
+import Navbar from "./components/Shared componenets/Navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/bretix/src/components/Shared componenets/Navbar.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
+const Navbar = () =>{
+    const navigate = useNavigate()
+    return(
+        <div>
+            <button onClick={()=>{navigate("/")}}>Home</button>
+        </div>
+    )
+}
+export default Navbar
