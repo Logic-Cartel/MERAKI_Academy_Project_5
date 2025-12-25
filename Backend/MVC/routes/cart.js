10047bc3bccabeae34f874b975f42266eead4847
MahmoudQasm <m.alshiekhqasem@gmail.com>
2025-12-26 01:13:24 +0300

    save cart changes before switching branch

--- Backend/MVC/routes/cart.js
+++ Backend/MVC/routes/cart.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const {
-  addToCart,
-  updateCart,
-  deleteFromCart,
-  checkout,
-} = require("../controllers/cart");
+const { updateCart } = require("../controllers/cart");
 const cartRouter = express.Router();
 
 //========================
-cartRouter.post("/", addToCart);
 
-cartRouter.put("/:cart_id", updateCart);
-
-cartRouter.delete("/:cart_id", deleteFromCart);
-
-cartRouter.post("/checkout", checkout);
+cartRouter.put("/:id", updateCart);
 
 //========================
 
