96b931a8e7fd47b74c60a2f202aa6058f43d5bad
MahmoudQasm <m.alshiekhqasem@gmail.com>
2025-12-29 01:39:28 +0300

    added

--- Backend/MVC/routes/categories.js
+++ Backend/MVC/routes/categories.js
@@ -4,12 +4,14 @@ const {
   getCategoryById,
   updateCategoryById,
   deleteCategoryById,
+  getAllCategories,
 } = require("../controllers/categories");
 const categoriesRouter = express.Router();
 
 //====================
 categoriesRouter.post("/addnewcategory", addNewCategory);
 categoriesRouter.get("/category/:id", getCategoryById);
+categoriesRouter.get("/", getAllCategories);
 categoriesRouter.put("/category/:id/update", updateCategoryById);
 categoriesRouter.delete("/category/:id", deleteCategoryById);
 //====================
