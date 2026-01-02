7e74774335b54577cdcf767ab6d701940fe41854
AhmadMAlhyari <ahmad.m.alhiyari@gmail.com>
2026-01-02 19:58:34 +0300

    dashboard for store owner

--- Frontend/bretix/src/components/Owners/StoreInfo.jsx
+++ Frontend/bretix/src/components/Owners/StoreInfo.jsx
@@ -1,30 +1,83 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+
 
 const StoreInfo = () => {
-  const { id } = useParams();
+  const navigate = useNavigate()
+  const { storeId } = useParams();
   const [storeInfo,setStoreInfo] = useState({})
   const [storeInfoEdition,setStoreInfoEdition] = useState({})
 
   useEffect(()=>{
     const getStoreInfo = async ()=>{
-        const result = await axios.get(`http://localhost:5000/stores/${id}`)
-        console.log(result.data.result[0]);
-        
+        const result = await axios.get(`http://localhost:5000/stores/${storeId}`)        
         setStoreInfo(result.data.result[0])
         setStoreInfoEdition(result.data.result[0])
     }
     getStoreInfo()
   },[])
+  
+  const confirm = async () => {
+  const result = await Swal.fire({
+    title: "Save Changes?",
+    text: "Are you sure you want to update info?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#1a3c34",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, Update",
+    cancelButtonText: "Cancel",
+  });
+
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: "Updating...",
+      text: "Please wait while we save your changes.",
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      didOpen: () => {
+        Swal.showLoading(); 
+      },
+    });
+
+    try {
+      await axios.put(
+        `http://localhost:5000/stores/${storeId}/update`,
+        { ...storeInfoEdition }
+      );
+      
+      setStoreInfo(storeInfoEdition);
+
+      Swal.fire({
+        title: "Success!",
+        text: "Product updated successfully.",
+        icon: "success",
+        confirmButtonColor: "#1a3c34",
+        timer: 1500
+      });
+
+    } catch (err) {
+      console.log(err);
+      Swal.fire({
+        title: "Error!",
+        text: "Could not update the product.",
+        icon: "error",
+        confirmButtonColor: "#1a3c34"
+      });
+    }
+  }
+};
+    
 
   return (<div>
     <img src={storeInfoEdition.logo} alt={storeInfoEdition.title} /><br />
     Title<input type="text" value={storeInfoEdition.title} onChange={(e)=>{setStoreInfoEdition({...storeInfoEdition,title:e.target.value})}} /> <br />
     Logo URL<input type="text" value={storeInfoEdition.logo} onChange={(e)=>{setStoreInfoEdition({...storeInfoEdition,logo:e.target.value})}} /> <br />
     Description <input type="text" value={storeInfoEdition.description} onChange={(e)=>{setStoreInfoEdition({...storeInfoEdition,description:e.target.value})}} />
-    {JSON.stringify(storeInfo) !== JSON.stringify(storeInfoEdition) && <button>Confirm Edition</button>} <button>Cancel</button>
+    {JSON.stringify(storeInfo) !== JSON.stringify(storeInfoEdition) && <button onClick={confirm}>Confirm Edition</button>} <button onClick={()=>{navigate(-1)}}>Cancel</button>
 
   </div>)
 };
